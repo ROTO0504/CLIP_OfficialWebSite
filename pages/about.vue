fce6a7b34462dc445ddef397092c4d4b23cf9ef2
ROTO0504 <0504hiroosa@gmail.com>
2022-12-26 16:16:36 +0900

    ノートpcに作業移すよう

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,22 @@
 <template>
   <v-card
-  elevation="2"
-  shaped>
-
-  <v-card-title class="text-h6">
-    TeaserPV
-  </v-card-title>
+    elevation="2"
+    shaped
+  >
+    <v-card-title class="text-h6">
+      TeaserPV
+    </v-card-title>
 
     <div style="padding:50% 0 0 0;position:relative;">
-      <iframe src="https://player.vimeo.com/video/782086165?h=5d7d8d5007&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TeaserPV">
-      </iframe>
+      <iframe
+        src="https://player.vimeo.com/video/782086165?h=5d7d8d5007&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+        allow="autoplay;
+              fullscreen; picture-in-picture"
+        allowfullscreen
+        style="position:absolute;top:0;left:0;width:100%;height:100%;"
+        frameborder="0"
+        title="TeaserPV"
+      />
     </div>
   </v-card>
 </template>
@@ -18,4 +25,3 @@
 export default {
   name: 'InspirePage'
 }
-</script>
